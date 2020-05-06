107e27793b3715a34a084a861e65ee9e820484ce
Risus <gesangcx@outlook.com>
2020-05-06 20:21:03 +0800

    a

--- src/main/java/com/coding/springAnno/config/MyConfig.java
+++ src/main/java/com/coding/springAnno/config/MyConfig.java
@@ -3,9 +3,11 @@ package com.coding.springAnno.config;
 import com.coding.springAnno.bean.Person;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 @PropertySource(value = {"classpath:person.properties"})
+@EnableAspectJAutoProxy
 @Configuration
 public class MyConfig {
 
